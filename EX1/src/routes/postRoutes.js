a12bcf94fa4f1a7c7e4419803b7a956157de738d
Kyte Hoang <htk09032003@gmail.com>
2025-10-24 15:18:17 +0700

    init BE ass

--- EX1/src/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { postController } from "../controllers/postController.js";
-
-const router = express.Router();
-
-router.get("/author/:id", postController.getPostByAuthorId);
-
-export default router;
